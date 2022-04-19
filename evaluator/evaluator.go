bb0aede1dc83bce46764bebeb42f35c765c1526c
Tomoki Sugiura @ Artificial Retina Development Office <tomoki_sugiura@nidek.co.jp>
2022-04-19 13:53:33 +0900

    add error object

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,7 +15,7 @@ func Eval(node ast.Node) object.Object {
   switch node := node.(type) {
 
   case *ast.Program:
-    return evalStatements(node.Statements)
+    return evalProgram(node)
 
   case *ast.ExpressionStatement:
     return Eval(node.Expression)
@@ -35,8 +35,43 @@ func Eval(node ast.Node) object.Object {
   case *ast.Boolean:
     return nativeBoolToBooleanObject(node.Value)
 
+  case *ast.BlockStatement:
+    return evalBlockStatement(node)
+
+  case *ast.IfExpression:
+    return evalIfExpression(node)
+
+  case *ast.ReturnStatement:
+    val := Eval(node.ReturnValue)
+    return &object.ReturnValue{Value: val}
+
   }
   return nil
+}
+
+func evalProgram(program *ast.Program) object.Object {
+  var result object.Object
+
+  for _, statement := range program.Statements {
+    result = Eval(statement)
+    if returnValue, ok := result.(*object.ReturnValue); ok {
+      return returnValue.Value
+    }
+  }
+  return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+  var result object.Object
+
+  for _, statement := range block.Statements {
+    result = Eval(statement)
+
+    if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+      return result
+    }
+  }
+  return result
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
@@ -44,8 +79,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
   for _, statement := range stmts {
     result = Eval(statement)
+    if returnValue, ok := result.(*object.ReturnValue); ok {
+      return returnValue.Value
+    }
   }
-
   return result
 }
 
@@ -96,6 +133,10 @@ func evalInfixExpression(
   switch {
   case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
     return evalIntegerInfixExpression(operator, left, right)
+  case operator == "==":
+    return nativeBoolToBooleanObject(left == right)
+  case operator == "!=":
+    return nativeBoolToBooleanObject(left != right)
   default:
     return NULL
   }
@@ -130,3 +171,28 @@ func evalIntegerInfixExpression(
   }
 }
 
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+  condition := Eval(ie.Condition)
+
+  if isTruthy(condition) {
+    return Eval(ie.Consequence)
+
+  } else if ie.Alternative != nil {
+    return Eval(ie.Alternative)
+  } else {
+    return NULL
+  }
+}
+
+func isTruthy (obj object.Object) bool {
+  switch obj {
+  case NULL:
+    return false
+  case TRUE:
+    return true
+  case FALSE:
+    return false
+  default:
+    return true
+  }
+}
